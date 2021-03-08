ba8d4d6a042f756b71e740744ea7285ae02a3848
Petr Abakumov <petr-abakumow@yandex.ru>
2021-03-09 00:47:56 +0300

    feat: final

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 console.log('Привет, Мир');
+
+const TIME_LIMIT = 1800;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.querySelector(".timer").innerHTML = formatTime(
+      timeLeft
+    );
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
